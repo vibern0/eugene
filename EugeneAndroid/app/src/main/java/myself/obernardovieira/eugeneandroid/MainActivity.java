3c9a20fc51f4156da7431a5026ecb15605982f6e
Bernardo Vieira <bernardo5@live.com.pt>
2017-05-28 00:37:05 +0100

    better app

--- EugeneAndroid/app/src/main/java/myself/obernardovieira/eugeneandroid/MainActivity.java
+++ EugeneAndroid/app/src/main/java/myself/obernardovieira/eugeneandroid/MainActivity.java
@@ -15,6 +15,8 @@ import java.net.UnknownHostException;
 public class MainActivity extends Activity {
 
     boolean connected = false;
+    Socket clientSocket;
+    DataOutputStream outToServer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,39 +32,75 @@ public class MainActivity extends Activity {
             new Thread(new Runnable() {
                 @Override
                 public void run() {
-
                     try {
                         //String sentence;
                         //String modifiedSentence;
                         //BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-                        Socket clientSocket = new Socket("192.168.4.1", 5678);
-                        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-                        connected = true;   
+                        clientSocket = new Socket("192.168.4.1", 5678);
+                        outToServer = new DataOutputStream(clientSocket.getOutputStream());
+                        connected = true;
                         //BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                         //sentence = inFromUser.readLine();
                         outToServer.writeBytes("hello" + '\n');
+                        outToServer.flush();
                         //modifiedSentence = inFromServer.readLine();
                         //System.out.println("FROM SERVER: " + modifiedSentence);
-                        clientSocket.close();
+
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-
                 }
             }).start();
         }
-
 
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    clientSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
     }
 
     public void onClickLigar(View v) {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    outToServer.writeBytes("play" + '\n');
+                    outToServer.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
     }
 
     public void onClickDesligar(View v) {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    outToServer.writeBytes("stop" + '\n');
+                    outToServer.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
     }
 }
