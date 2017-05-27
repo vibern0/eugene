be8e05bbdcb2d0433265b23726dd2ddd7cfcd792
Bernardo Vieira <bernardo5@live.com.pt>
2017-05-28 00:02:23 +0100

    from android to esp

--- EugeneAndroid/app/src/main/java/myself/obernardovieira/eugeneandroid/MainActivity.java
+++ EugeneAndroid/app/src/main/java/myself/obernardovieira/eugeneandroid/MainActivity.java
@@ -2,39 +2,67 @@ package myself.obernardovieira.eugeneandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class MainActivity extends Activity {
+
+    boolean connected = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                String serverName = "192.168.4.15";
-                int port = 8888;
-                try {
-                    System.out.println("Connecting to " + serverName + " on port " + port);
-                    Socket client = new Socket(serverName, port);
-                    System.out.println("Just connected to " + client.getRemoteSocketAddress());
-                    OutputStream outToServer = client.getOutputStream();
-                    DataOutputStream out = new DataOutputStream(outToServer);
-                    //
-                    out.writeUTF("Hello from");
-                    client.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if(!connected) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+
+                    try {
+                        //String sentence;
+                        //String modifiedSentence;
+                        //BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+                        Socket clientSocket = new Socket("192.168.4.1", 5678);
+                        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+                        connected = true;   
+                        //BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                        //sentence = inFromUser.readLine();
+                        outToServer.writeBytes("hello" + '\n');
+                        //modifiedSentence = inFromServer.readLine();
+                        //System.out.println("FROM SERVER: " + modifiedSentence);
+                        clientSocket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
                 }
-            }
-        }).start();
+            }).start();
+        }
 
 
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    public void onClickLigar(View v) {
+    }
+
+    public void onClickDesligar(View v) {
     }
 }
